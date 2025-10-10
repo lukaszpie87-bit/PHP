cb4d44cbc75bde1a74acfc199163206e641a4cd3
lukaszpie87-bit <lukaszpie87@gmail.com>
2025-10-10 13:54:21 +0200

    dziedziczenie

---
+++ czas1.php
@@ -0,0 +1,6 @@
+<?php
+
+$czas = mktime(11,14,54,8,12,2014);
+echo "Created date is " . date( "d.m.Y h:i:sa", $czas);
+
+?>
---
+++ urodzenie.php
@@ -0,0 +1,4 @@
+<?php
+$czas = mktime(11,22,31,3,30,2006);
+echo date("d.m.Y h:i:sa", $czas)
+?>
